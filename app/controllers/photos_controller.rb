053076d03930c4e8c16f504533e1250cdb15ce7d
Jared Hutchinson <jared.hutchinson99@gmail.com>
2017-09-27 12:08:07 +1000

    Add like button

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -40,36 +40,46 @@ class PhotosController < ApplicationController
 
   # PATCH/PUT /photos/1
   # PATCH/PUT /photos/1.json
-  def update
-    respond_to do |format|
-      if @photo.update(photo_params)
-        format.html { redirect_to @photo, notice: 'Photo was successfully updated.' }
-        format.json { render :show, status: :ok, location: @photo }
-      else
-        format.html { render :edit }
-        format.json { render json: @photo.errors, status: :unprocessable_entity }
+    def update
+      respond_to do |format|
+        if is_liking?
+          @photo.toggle_liked_by(current_user)
+          format.html { redirect_to @photo }
+          format.json { render :show, status: :ok, location: @photo }
+          
+        if @photo.update(photo_params)
+          format.html { redirect_to @photo, notice: 'Photo was successfully updated.' }
+          format.json { render :show, status: :ok, location: @photo }
+        else
+          format.html { render :edit }
+          format.json { render json: @photo.errors, status: :unprocessable_entity }
+        end
       end
     end
   end
-
   # DELETE /photos/1
   # DELETE /photos/1.json
-  def destroy
-    @photo.destroy
-    respond_to do |format|
-      format.html { redirect_to photos_url, notice: 'Photo was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_photo
-      @photo = Photo.find(params[:id])
+    def destroy
+      @photo.destroy
+      respond_to do |format|
+        format.html { redirect_to photos_url, notice: 'Photo was successfully destroyed.' }
+        format.json { head :no_content }
+      end
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def photo_params
-      params.require(:photo).permit(:image, :description)
-    end
+    private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_photo
+        @photo = Photo.find(params[:id])
+      end
+
+      # Never trust parameters from the scary internet, only allow the white list through.
+      def photo_params
+        params.require(:photo).permit(:image, :description)
+      end
+
+      def is_liking?
+        # Is there a 'liked' field in the form?
+        params.require(:photo)[:liked].present?
+      end
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,4 +2,28 @@ class Photo < ApplicationRecord
   include ImageUploader::Attachment.new(:image)
   
   belongs_to :user
+  has_and_belongs_to_many :likers, class_name: 'User', join_table: :likes
+
+# photo = Photo.first
+# all_people_who_liked_that_photo = photo.:likers
+
+# other_person = User.second
+# # To make 'other_person' like 'photo':
+# photo.likers << other_person # When they click the like button
+# photos.likers.destroy(other_person)
+
+# 
+  def liked_by?(user)
+    likers.exists?(user.id)
+  end
+
+  def toggle_liked_by(user)
+    # If photo has been liked by user
+    if likers.exists?(user.id)
+      likers.destroy(user.id)
+    else
+      likers << user
+    end
+  end
+
 end
