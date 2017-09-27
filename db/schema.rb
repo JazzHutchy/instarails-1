bc1cb4d5fdcb7b1400346cd5351755e14ba4fd68
Jared Hutchinson <jared.hutchinson99@gmail.com>
2017-09-27 14:59:40 +1000

    added fixes to destroy and edit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170927003420) do
+ActiveRecord::Schema.define(version: 20170927021127) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "photo_id"
+    t.integer "user_id"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["photo_id"], name: "index_comments_on_photo_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "likes", id: false, force: :cascade do |t|
     t.integer "user_id", null: false
